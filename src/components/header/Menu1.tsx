8998799ace1922ceee8d75e7d745463bd4140ca7
amishk783 <amishkumar800@gmail.com>
2024-02-08 13:13:51 +0530

    update-1

---
+++ src/components/header/Menu1.tsx
@@ -0,0 +1,126 @@
+import Image from "next/image";
+
+const icons: { slink: string; image: string }[] = [
+  {
+    slink: "",
+    image: "/instagram.png",
+  },
+  {
+    slink: "",
+    image: "/facebook.png",
+  },
+  {
+    slink: "",
+    image: "/linkedin.png",
+  },
+  {
+    slink: "",
+    image: "/twitter.png",
+  },
+];
+const components: {
+  title: string;
+  href: string;
+  description: string;
+  image: string;
+}[] = [
+  {
+    title: "E-commerce",
+    href: "/docs/primitives/alert-dialog",
+    image: "/portfolio-mega-menu-1.jpg",
+    description: "we undertook a project to migrate their existing.",
+  },
+  {
+    title: "App Development",
+    href: "/docs/primitives/hover-card",
+    image: "/portfolio-mega-menu-2.jpg",
+    description: "The mobile application has significantly improved.",
+  },
+  {
+    title: "SAAS Integration",
+    href: "/docs/primitives/progress",
+    image: "/portfolio-mega-menu-3.jpeg",
+    description: "We partnered with DEF to upgrade their outdated IT.",
+  },
+  {
+    title: "Virtual Reality",
+    href: "/docs/primitives/scroll-area",
+    image: "/portfolio-mega-menu-4.jpeg",
+    description: "Enter into the virtual reality world for real experience.",
+  },
+];
+
+export function Menu1() {
+  return (
+    <div className="absolute left-0 w-full h-100vh bg-blue-200 overflow-x-hidden ">
+      {" "}
+      {/* outline */}
+      <div className="">
+        <div className="max-container flex ">
+          <div className="flex-1 pt-10 pr-[95px]">
+            <div className=" py-7 w-full items-center justify-between flex-col space-y-4">
+              <div className="w-full grid grid-cols-2 gap-5 space-y-2 border-b-2  items-start pb-16 justify-between">
+                {" "}
+                {/* grid */}
+                {/* {components.map((component) => (
+                  <div
+                    key={component.title}
+                    className=" text-base font-normal flex gap-6 space-y-2 "
+                  >
+                    <div className="border-purple-100 max-w-56 w-full rounded-xl ">
+                      <Image
+                        src={component.image}
+                        alt={component.title}
+                        width={220}
+                        height={100}
+                      />
+                    </div>
+                    <div className="">
+                      <h3>
+                        <a href="">{component.title}</a>
+                      </h3>
+                      <p>{component.description}</p>
+                    </div>
+                  </div>
+                ))} */}
+              </div>
+              <ul className="flex gap-6">
+                {" "}
+                {/* icons  */}
+                {icons.map((icon) => (
+                  <Image
+                    key={icon.slink}
+                    src={icon.image}
+                    alt={icon.slink}
+                    width={25}
+                    height={50}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className=" bg-white relative py-12 pl-1">
+            {" "}
+            {/* right part  */}
+            <div className="flex flex-col items-start">
+              <Image
+                src="/macbook.jpg"
+                alt=""
+                className="block border-4 border-white mb-7 max-w-full rounded-lg"
+                width={300}
+                height={300}
+              />
+              <h2 className=" mb-2 text-2xl font-medium">Solution in Mind</h2>
+              <p className="text-base mb-4 font-normal">
+                Download slack in your mobile for <br /> your daily usage.
+              </p>
+              <a className=" inline-bloc text-cyan-500 font-medium text-base border-b-2 border-cyan-400">
+                View more
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -1,41 +1,74 @@
-import { LayoutNav } from "@/lib/LayoutNav";
-import { ChevronDown } from "lucide-react";
+import { useRef, useState } from "react";
 
-const NavLinks: { service: string }[] = [
+import { Menu2 } from "@/lib/Menu2";
+import { ChevronDown } from "lucide-react";
+import { Menu0 } from "./Menu0";
+import { Menu3 } from "./Menu3";
+import { Menu1 } from "./Menu1";
+
+const NavLinks: { id: number; service: string }[] = [
   {
+    id: 1,
     service: "Home",
   },
   {
+    id: 2,
     service: "Company",
   },
   {
+    id: 3,
     service: "PortFolio",
   },
   {
+    id: 4,
     service: "Services",
   },
 ];
 
 export function NavBar() {
-
-  // const [selected, setService] = useState<>();
+  const [hovering, setHovering] = useState<number | null>(null);
+  // const [popoverLeft, setPopoverLeft] = useState<number | null>(null);
+  // const [popoverHeight, setPopoverHeight] = useState<number | null>(null);
+  const [isLayoutOpen, setIsLayoutOpen] = useState<boolean>(false);
 
   return (
-    <ul className="flex xl:flex-row gap-10 list-none py-6">
-      {NavLinks.map((navlink) => (
-        <li
-          key={navlink.service}
-          className="px-2 py-1 rounded-full space-y-10 hover:bg-zinc-400  hover:bg-opacity-20"
-        >
-          <span className="flex gap-1">
-            {navlink.service} <ChevronDown />
-          </span>
-
-          <LayoutNav/>
-        </li>
-      ))}
-      <li></li>
-      <li></li>
-    </ul>
+    <nav onMouseEnter={() => setHovering(null)}>
+      <ul className="flex xl:flex-row gap-10 list-none py-6">
+        {NavLinks.map((navlink) => (
+          <li
+            key={navlink.service}
+            onMouseEnter={(event) => setHovering(navlink.id)}
+            className="px-2 py-1 rounded-full space-y-10 hover:bg-zinc-400  hover:bg-opacity-20"
+          >
+            <span
+              onMouseEnter={() => {
+                setIsLayoutOpen(true);
+              }}
+              onMouseLeave={() => {
+                setIsLayoutOpen(false);
+              }}
+              className="flex gap-1"
+            >
+              {navlink.service} <ChevronDown />
+            </span>
+            {hovering === 0 ? (
+              <Menu0 />
+            ) : hovering === 1 ? (
+              <Menu1 />
+            ) : hovering === 2 ? (
+              <Menu2 />
+            ) : hovering === 3 ? (
+              <Menu3 />
+            ) : null}
+            {/* {typeof hovering === "number" && (
+              <div>
+               
+              </div>
+            )} */}
+            {/* {isLayoutOpen && <Menu2 />} */}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 }
